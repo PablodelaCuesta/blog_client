d62d4c2630bc205d42d62aed583e8f0d8b3b156e
Pablo de la Cuesta <pablodelacuesta@hotmail.es>
2022-04-12 18:34:13 +0200

    staging connections

--- src/Core/constants/connections.js
+++ src/Core/constants/connections.js
@@ -1,4 +1,3 @@
-// export const BASE_API = 'https://api.pablodelacuesta.es/api'
-// export const BASE = 'https://api.pablodelacuesta.es'
-export const BASE = process.env.BASE || "http://localhost:8080"
-export const BASE_API = process.env.BASE_API || "http://localhost:8080/api"
+
+export const BASE = 'http://api.pablodelacuesta.es' 
+export const BASE_API = 'http://api.pablodelacuesta.es/api'
